23a956797b2fe98583852f200623cdb3bc5a67f1
MaJinWoo <maddonge@naver.com>
2022-06-21 17:31:31 +0900

    forth

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-i2btdncg_%gs#3@bwtg=j4ctyj^7iee+)vy!4x&tw(#yordr+@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
